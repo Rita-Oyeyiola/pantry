c1b990fff4c0928c7a5d416c767f2dc7e099d868
Rita-Oyeyiola <oyeyiolarita221@gmail.com>
2025-03-12 11:16:17 +0100

    updated pantry and basket api

--- src/main/java/com/atos/pantry/service/BasketService.java
+++ src/main/java/com/atos/pantry/service/BasketService.java
@@ -1,26 +1,31 @@
 package com.atos.pantry.service;
 
+import com.atos.pantry.configuration.PantryConfig;
 import com.atos.pantry.model.Basket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class BasketService {
-    public final WebClient webClient;
 
-    public BasketService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl("https://api.yourpantry.com").build();
+    public final PantryConfig pantryConfig;
+    @Autowired
+    public BasketService(PantryConfig pantryConfig) {
+        this.pantryConfig = pantryConfig;
     }
 
     public Flux<Basket> getBasketsByPantryId (String pantryId) {
-        return webClient.get()
+        return this.pantryConfig.webClient().get()
                 .uri("/pantry/{pantryId}/basket", pantryId)
                 .retrieve()
                 .bodyToFlux(Basket.class);
     }
 
     public Mono<Basket> createBasket(Basket basket) {
-        return webClient.post()
+        return this.pantryConfig.webClient().post()
                 .uri("/pantry/pantryId/basket/{basketName}", basket.getPantryId(), basket.getName())
                 .bodyValue(basket)
                 .retrieve()
@@ -28,7 +33,7 @@ public class BasketService {
     }
 
     public Mono<Basket> updateBasket(String id, Basket basket) {
-        return webClient.put()
+        return this.pantryConfig.webClient().put()
                 .uri("/pantry/pantryId/basket/{basketName}", basket.getPantryId(), basket.getName())
                 .bodyValue(basket)
                 .retrieve()
@@ -36,7 +41,7 @@ public class BasketService {
     }
 
     public Mono<Void> deleteBasket(String pantryId, String basketName) {
-        return webClient.delete()
+        return this.pantryConfig.webClient().delete()
                 .uri("/pantry/{pantryId}/basket/{basketName}")
                 .retrieve()
                 .bodyToMono(Void.class);
